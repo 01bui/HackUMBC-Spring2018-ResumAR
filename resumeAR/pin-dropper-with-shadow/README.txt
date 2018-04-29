39c36b45300a393f2e82bd1f671630832e87e713
Vy Bui <01bui@cardinalmail.cua.edu>
2018-04-29 10:21:38 -0400

    commit

--- resumeAR/pin-dropper-with-shadow/README.txt
+++
@@ -1,3 +0,0 @@
-A Pen created at CodePen.io. You can find this one at https://codepen.io/maudulus/pen/ohFiH.
-
- This is a pin dropping pen, which allows you to drop pins anywhere on the page that has been clicked. 
--- resumeAR/resumeAR/js/index.js
+++ resumeAR/resumeAR/js/index.js
@@ -1,36 +1,26 @@
-pin = $(".pin");
-    $('canvas').click(function(e){
-      pin.clone().appendTo("body").attr("style", "top: " + (e.pageY - 78) + "px; left: " + (e.pageX - 36.5) + "px;");
-    
-      if (last_x == 0 && last_y == 0){
-        last_x = (e.pageY - 78);
-        last_y = (e.pageX - 36.5);
-      } else {
-        drawLineBetweenDrops((e.pageX - 36.5),(e.pageY - 78))
-      }
-      
-      pinshadow.clone().appendTo("body").attr("style", "top: " + (e.pageY-5) + "px; left: " + (e.pageX) + "px;");    
-});
+
 pinshadow = $(".pin-shadow");
     $('canvas').click(function(e){
     pinshadow.clone().appendTo("body").attr("style", "top: " + (e.pageY-5) + "px; left: " + (e.pageX) + "px;");
 });
 
-var last_x = 0;
-var last_y = 0;
-var new_x;
-var new_y;
-function drawLineBetweenDrops(x_coordinates,y_coordinates) {
-  new_x = x_coordinates;
-  new_y = y_coordinates;
-  var c = document.getElementById("myCanvas");
-  var ctx = c.getContext("2d");
-  ctx.beginPath();
-  ctx.moveTo(last_x,last_y);
-  ctx.lineTo(new_x,new_y);
-  ctx.strokeStyle = '#ff0000';
-  ctx.stroke();
-  
-  last_x = x_coordinates;
-  las_y = y_coordinates;
+
+var imageLoader = document.getElementById('imageLoader');
+imageLoader.addEventListener('change', handleImage, false);
+var canvas = document.getElementById('imageCanvas');
+var ctx = canvas.getContext('2d');
+
+
+function handleImage(e) {
+    var reader = new FileReader();
+    reader.onload = function (event) {
+        var img = new Image();
+        img.onload = function () {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            ctx.drawImage(img, 0, 0);
+        }
+        img.src = event.target.result;
+    }
+    reader.readAsDataURL(e.target.files[0]);
 }
